9e31ba918531babc84018667ee43929ac141f1fe
Andrew Nikonchuk <anikonch@gmail.com>
2018-06-13 15:41:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,9 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else
-    katz_deli.each_
+    katz_deli.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
   end
 end
