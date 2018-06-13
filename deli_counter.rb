fa0e04eb5280174662f442a4acd4ec7ec58484f1
Andrew Nikonchuk <anikonch@gmail.com>
2018-06-13 15:38:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,10 @@
 katz_deli = []
 
 def line(katz_deli)
+  line_array = []
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else
-    puts katz_deli
+    katz_deli.each_
   end
 end
